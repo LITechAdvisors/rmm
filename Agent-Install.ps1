9f6d35b5ea4b08e5bc288a5f36f4c76be49c5b73
LITechAdvisors <96080746+LITechAdvisors@users.noreply.github.com>
2023-05-17 11:34:03 -0400

    Update Agent-Install.ps1

--- Agent-Install.ps1
+++ Agent-Install.ps1
@@ -1,4 +1,3 @@
-[CmdletBinding()]
 param (
     [Parameter(Mandatory = $true)]
     [string]$Location,
@@ -7,64 +6,66 @@ param (
     [string]$Key
 )
 
-# Step 2: Check if services are running
-Write-Host "Looking for services to see if they are running..."
-$services = Get-Service | Where-Object {$_.Name -eq "ITSPlatform" -or $_.Name -eq "ITSPlatformManager"}
+& {
+    # Step 2: Check if services are running
+    Write-Host "Looking for services to see if they are running..."
+    $services = Get-Service | Where-Object {$_.Name -eq "ITSPlatform" -or $_.Name -eq "ITSPlatformManager"}
 
-if ($services) {
-    Write-Host "Services found:"
-    $services | Format-Table Name, Status
-    Write-Host
-    # Step 3: Start services if they are stopped
-    $stoppedServices = $services | Where-Object {$_.Status -eq "Stopped"}
-    if ($stoppedServices) {
-        Write-Host "The following services are stopped and will be started:"
-        $stoppedServices | ForEach-Object {
-            Write-Host $_.Name
-            try {
-                Start-Service $_.Name -ErrorAction Stop
-                Write-Host "Service started."
-            } catch {
-                Write-Host "Failed to start the service."
-            }
-        }
+    if ($services) {
+        Write-Host "Services found:"
+        $services | Format-Table Name, Status
         Write-Host
+        # Step 3: Start services if they are stopped
+        $stoppedServices = $services | Where-Object {$_.Status -eq "Stopped"}
+        if ($stoppedServices) {
+            Write-Host "The following services are stopped and will be started:"
+            $stoppedServices | ForEach-Object {
+                Write-Host $_.Name
+                try {
+                    Start-Service $_.Name -ErrorAction Stop
+                    Write-Host "Service started."
+                } catch {
+                    Write-Host "Failed to start the service."
+                }
+            }
+            Write-Host
+        }
+        else {
+            Write-Host "All services are already running."
+        }
     }
     else {
-        Write-Host "All services are already running."
+        # Step 4: Create Support directory
+        Write-Host "Service not found."
+        Write-Host "Creating Support directory."
+        $supportDir = "C:\Support"
+        if (-not (Test-Path $supportDir)) {
+            New-Item -ItemType Directory -Path $supportDir | Out-Null
+            Write-Host "Support directory created."
+        }
+
+        # Step 5: Download file
+        $fileUrl = "https://drive.google.com/uc?id=1D-0qpQYxgFEukqs2fsF2ZybIu1d8X72N&authuser=0&export=download&confirm=t&uuid=6a923679-d2a0-4d66-b8c8-94171796735d&at=AKKF8vyXz8agiJg36W8zUHoYWzHv:1684328105399"
+
+        $constantFileName = "Windows_OS_ITSPlatform_"
+        $fileName = "C:\Support\$Location`_$constantFileName`TKN$Key.msi"
+
+        # Check if the file already exists and overwrite it
+        if (Test-Path $fileName) {
+            Write-Host "File already exists. Overwriting..."
+            Remove-Item $fileName -Force
+        }
+
+        # Download the file
+        Write-Host "Downloading file..."
+        Invoke-WebRequest -Uri $fileUrl -OutFile $fileName -Quiet
+
+        # Step 9: Silently install MSI file
+        Write-Host "Installing MSI file..."
+        Start-Process -FilePath msiexec.exe -ArgumentList "/i `"$fileName`" /qn" -Wait
+        Write-Host "Installation completed."
+
+        # Step 10: Print script completion message
+        Write-Host "Script execution completed."
     }
-}
-else {
-    # Step 4: Create Support directory
-    Write-Host "Service not found."
-    Write-Host "Creating Support directory."
-    $supportDir = "C:\Support"
-    if (-not (Test-Path $supportDir)) {
-        New-Item -ItemType Directory -Path $supportDir | Out-Null
-        Write-Host "Support directory created."
-    }
-
-    # Step 5: Download file
-    $fileUrl = "https://drive.google.com/uc?id=1D-0qpQYxgFEukqs2fsF2ZybIu1d8X72N&authuser=0&export=download&confirm=t&uuid=6a923679-d2a0-4d66-b8c8-94171796735d&at=AKKF8vyXz8agiJg36W8zUHoYWzHv:1684328105399"
-
-    $constantFileName = "Windows_OS_ITSPlatform_"
-    $fileName = "C:\Support\$Location`_$constantFileName`TKN$Key.msi"
-
-    # Check if the file already exists and overwrite it
-    if (Test-Path $fileName) {
-        Write-Host "File already exists. Overwriting..."
-        Remove-Item $fileName -Force
-    }
-
-    # Download the file
-    Write-Host "Downloading file..."
-    Invoke-WebRequest -Uri $fileUrl -OutFile $fileName -Quiet
-
-    # Step 9: Silently install MSI file
-    Write-Host "Installing MSI file..."
-    Start-Process -FilePath msiexec.exe -ArgumentList "/i `"$fileName`" /qn" -Wait
-    Write-Host "Installation completed."
-
-    # Step 10: Print script completion message
-    Write-Host "Script execution completed."
 }
